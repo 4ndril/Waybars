75b756d1dc174886b883cfd52655fff417f3a241
Andril <kingbeef@gmail.com>
2024-09-29 08:11:27 -0500

    Add files via upload

--- default-mod/config.sh
+++ default-mod/config.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 theme_name="Default Mod"
+#Created by Stephan Raabe  https://www.ml4w.com/ & https://github.com/mylinuxforwork - customized by Andril https://mastodon.xyz/@andril & https://github.com/4ndril/Waybars with some borrowed elements -Enjoy!
